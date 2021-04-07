4e1876ebe7b8d3ff016ce0c2b93e13befbb751f6
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2021-04-07 19:51:06 +0800

    Update Heroku.php

--- platform/Heroku.php
+++ platform/Heroku.php
@@ -103,7 +103,7 @@ function setConfig($arr, $disktag = '')
 {
     if ($disktag=='') $disktag = $_SERVER['disktag'];
     $disktags = explode("|",getConfig('disktag'));
-	error_log1(getenv($disktag) . '<br>' . json_encode(getenv($disktag)));
+	error_log1($disktag . '<br>' . getenv($disktag));
     $diskconfig = json_decode(getenv($disktag), true);
     $tmp = [];
     $indisk = 0;
